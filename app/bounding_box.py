a1c8ad5d8aca6aa633b31439d076ad19693e22ea
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-06-01 20:35:44 -0230

    bounding boxes now drawn around parent and labeled, need to add remaining textract blocks

--- app/bounding_box.py
+++ app/bounding_box.py
@@ -1,7 +1,7 @@
-from PIL import ImageDraw, Image
+from PIL import ImageDraw, Image, ImageFont
 from pdf2image.pdf2image import convert_from_path
 import json
-
+import trp
 
 def draw_boxes(file, output_path, blocks_path, line_width=4):
     """Draws bounding boxes on a PDF file."""
@@ -14,30 +14,41 @@ def draw_boxes(file, output_path, blocks_path, line_width=4):
     img_height = image.height
 
     with open(blocks_path, 'r') as f:
-        blocks = json.load(f)['Blocks']
+        doc = trp.Document(json.load(f))
 
     draw = ImageDraw.Draw(image)
-    for block in blocks:
-        points = []
-        for p in block['Geometry']['Polygon']:
-            points.append((int(p['X'] * img_width), int(p['Y'] * img_height)))
+    font = ImageFont.truetype('documents/fonts/OpenSans-Regular.ttf', line_width*6)
 
-        if 'BoundingBox' in block['Geometry']:
-            box = block['Geometry']['BoundingBox']
-            left = int(img_width * box['Left'])
-            top = int(img_height * box['Top'])
-            width = int(img_width * box['Width'])
-            height = int(img_height * box['Height'])
+    color_dict = {"LINE": "Red", "TABLE": "Blue"}
 
-            points.append((left, top))
-            points.append((left + width, top))
-            points.append((left + width, top + height))
-            points.append((left, top + height))
+    for page in doc.pages:
+        for line in page.lines + page.tables:
+            box = line.geometry.boundingBox
+            left = int(img_width * box.left)
+            top = int(img_height * box.top)
+            width = int(img_width * box.width)
+            height = int(img_height * box.height)
 
-        draw.line(points, fill='Red', width=line_width)
+            points = [(left, top), (left + width, top), (left + width, top + height), (left, top + height)]
+
+            # Using different color for different type of blocks
+            draw.polygon(points, outline=color_dict[line.blockType], width=line_width)
+            
+            # Measure the size of the text using textbbox
+            text_bbox = draw.textbbox((left, top), line.blockType, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            
+            # Draw a rectangle as a background for the text
+            draw.rectangle((left+2, top+2, left + text_width, top + text_height), fill="Blue")
+            
+            # Draw the text itself
+            draw.text((left, top), line.blockType, fill='White', font=font)
 
     # save image to output path
     image.save(output_path, 'PDF', resolution=100.0)
 
 # Example usage
-draw_boxes('documents/test2.pdf', 'app/textract_results/output2_bb.pdf', 'app/textract_results/output2.json')
+draw_boxes('documents/test2.pdf', 'app/bounding_box_results/output3_bb.pdf', 'app/textract_results/output2.json')
+
+
