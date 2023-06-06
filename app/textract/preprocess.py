6652d61da582763bcf66dc6da50475d2c5f5817c
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-06-06 16:04:42 -0230

    testing preprocessing methods

--- app/textract/preprocess.py
+++ app/textract/preprocess.py
@@ -2,43 +2,48 @@
 This file is used to preprocess the pdf documents before running them through AWS Textract.
 
 This includes deskewing the document, and converting it to a png file.
+
 """
 
-from pdf2image.pdf2image import convert_from_path
-import cv2
-import numpy as np
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract as pt
+import os
 
-def convert_pdf_to_png(pdf_path, output_path):
-    """Converts a PDF file to a PNG file."""
-    pages = convert_from_path(pdf_path, 500)
-    pages[0].save(output_path, 'PNG')
+def pdf_to_png(pdf_path, png_path='app/png_results'):
+    """
+    Converts a pdf file to a png file.
+    """
 
-def rotate_image(image, angle):
-    """Rotates an image by a given angle."""
-    height, width = image.shape[:2]
-    center = (width / 2, height / 2)
-    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, rotation_matrix, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
+    if not os.path.exists(png_path):
+        os.makedirs(png_path)
 
-def deskew_image(pdf_path, output_path):
-    """Deskews an image and saves output as a PNG file in the app folder."""
-    app_output_path = f'{output_path}.png'
-    convert_pdf_to_png(pdf_path, app_output_path)
-    image = cv2.imread(app_output_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    coords = np.column_stack(np.where(thresh > 0))
-    # angle = cv2.minAreaRect(coords)[-1]
-    # if angle < -45:
-    #     angle = -(90 + angle)
-    # else:
-    #     angle = -angle
-    angle = -1
-    rotated = rotate_image(image, angle)
-    cv2.imwrite(app_output_path, rotated)
+    
+    # This will return a list of PIL Image objects, one for each page of the pdf
+    images = convert_from_path(pdf_path)
+
+    for i, image in enumerate(images):
+        # We are going to save each page as a separate png image
+        # png_path should be a directory, not a file path
+        image_path = f"{png_path}/page_{i+1}.png"
+        image.save(image_path, 'PNG')
+
+    print(f"Converted {len(images)} pages.")
+
+
+def get_image_info(png_path):
+    """
+    gets information about image for preprocessing
+    """
+    image = Image.open(png_path)
+    image_info = pt.image_to_osd(image)
+    return image_info
+
+
+print(get_image_info('app/png_results/test/page_1.png'))
+
+
     
 
-# Example usage
-deskew_image('documents/test.pdf', 'documents/test_deskewed')
+
+    
