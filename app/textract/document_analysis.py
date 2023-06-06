d5b05192d97e1adf92517bf8d11f1ad38683210b
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-06-06 17:18:31 -0230

    testing pipeline

--- app/textract/document_analysis.py
+++ app/textract/document_analysis.py
@@ -3,8 +3,8 @@ A User interface where you input a pdf and get the json results and a pdf with b
 """
 
 from .textract import process_pdf
-from .textract_text import process_pdf as process_pdf_text
 from .bounding_box import draw_boxes
+from .preprocess import deskew_pdf
 from .ctrp import Document
 
 import pandas as pd
@@ -12,11 +12,8 @@ import pandas as pd
 
 def run_document_analysis(filepath, bucket, filename, json_path):
     """Runs AWS Textract analysis on a PDF file."""
-    return process_pdf(filepath, bucket, filename, json_path)
-
-def run_text_detection(filepath, bucket, filename, json_path):
-    """Runs AWS Textract text detection on a PDF file."""
-    return process_pdf_text(filepath, bucket, filename, json_path)
+    pp_filepath = deskew_pdf(filepath, filename)
+    return process_pdf(pp_filepath, bucket, filename, json_path)
 
 def table_to_csv(blocks, output_path):
     """Converts a table in a PDF file to a CSV file."""
@@ -34,20 +31,16 @@ def table_to_csv(blocks, output_path):
     df.to_csv(output_path, index=False)
 
 
-def draw_bounding_boxes(filepath, bucket, filename, output_path, json_path, analysis=True):
+def draw_bounding_boxes(filepath, bucket, filename, output_path, json_path):
     """Draws bounding boxes on a PDF file."""
-    if analysis:
-        blocks = run_document_analysis(filepath, bucket, filename, json_path)
-        table_to_csv(blocks, 'app/table_results/output.csv')
-        draw_boxes(filepath, output_path, blocks)
-    else:
-        blocks = run_text_detection(filepath, bucket, filename, json_path)
-        table_to_csv(blocks, 'app/table_results/output.csv')
-        draw_boxes(filepath, output_path, blocks)
+    blocks = run_document_analysis(filepath, bucket, filename, json_path)
+    table_to_csv(blocks, 'app/results/table_results/output.csv')
+    draw_boxes(filepath, output_path, blocks)
+    
 
 
 # Example usage
 if __name__ == '__main__':
-    draw_bounding_boxes('documents/tests/test3.pdf', 'pdf-to-text-aws', 'test3.pdf', 'app/bounding_box_results/output3.pdf', 'app/textract_results/output3.json')
-    table_to_csv('app/textract_results/output3.json', 'app/table_results/output3.csv')
+    draw_bounding_boxes('documents/tests/test2.pdf', 'pdf-to-text-aws', 'test2.pdf', 'app/results/bounding_box_results/output2.pdf', 'app/results/textract_results/output2.json')
+
     
